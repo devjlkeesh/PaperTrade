6e7322875642bbe33b82e9ec646c4049d6dacc92
Suhrob <55597631+suhrob-karimov@users.noreply.github.com>
2023-08-20 11:19:36 +0500

    inventory-product 2

--- src/main/java/dev/jlkeesh/papertrade/repository/main/CustomerPaymentReconciliationRepositoryImpl.java
+++ src/main/java/dev/jlkeesh/papertrade/repository/main/CustomerPaymentReconciliationRepositoryImpl.java
@@ -3,10 +3,13 @@ package dev.jlkeesh.papertrade.repository.main;
 import dev.jlkeesh.papertrade.criteria.CustomerPaymentReconciliationCriteria;
 import dev.jlkeesh.papertrade.dao.GenericDao;
 import dev.jlkeesh.papertrade.domains.main.CustomerPaymentReconciliation;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author : Suhrob Karimov
  * @since : 20/08/2023 - 09:37
  */
+
+@Repository
 public class CustomerPaymentReconciliationRepositoryImpl extends GenericDao<CustomerPaymentReconciliation, Long, CustomerPaymentReconciliationCriteria> implements CustomerPaymentReconciliationRepository {
 }
